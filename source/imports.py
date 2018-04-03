1e380344864186eeb1cf00b92731a79d4a755990
XAVIERLOI <xavierloizeau@free.fr>
2018-04-03 10:10:35 +0200

    Added a basic folder structure

---
+++ source/imports.py
@@ -0,0 +1,8 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
+
+import numpy as np
+import pandas as pd
+import missingno as msno
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="whitegrid")
